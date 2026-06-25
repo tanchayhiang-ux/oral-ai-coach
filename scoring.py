f57583a70f60a8bf8ddf1dffeff5f6e36f3e5c13
tanchayhiang-ux <tan_chay_hiang@moe.edu.sg>
2026-06-25 12:21:00 +0800

    Create scoring.py

---
+++ scoring.py
@@ -0,0 +1,12 @@
+def get_badge(score):
+
+    if score >= 25:
+        return "🏆 华文小冠军"
+
+    elif score >= 20:
+        return "🌟 表达高手"
+
+    elif score >= 15:
+        return "👍 努力之星"
+
+    return "💪 继续加油"
